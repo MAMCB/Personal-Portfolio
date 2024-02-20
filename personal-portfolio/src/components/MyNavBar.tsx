9ec3e3d807eaeb68895802526423027f0c91a9f8
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-02-20 20:21:09 +0100

    added page links on the navbar

--- personal-portfolio/src/components/MyNavBar.tsx
+++ personal-portfolio/src/components/MyNavBar.tsx
@@ -18,14 +18,14 @@ const MyNavBar = () => {
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
-        <Navbar.Link href="#" active>
+        <Navbar.Link href="/" active>
           Home
         </Navbar.Link>
-        <Navbar.Link as={Link} href="#">
-          About
+        <Navbar.Link as={Link} href="/projects">
+          Projects
         </Navbar.Link>
-        <Navbar.Link href="#">Services</Navbar.Link>
-        <Navbar.Link href="#">Pricing</Navbar.Link>
+        <Navbar.Link href="/about">About me</Navbar.Link>
+        <Navbar.Link href="/cv">CV</Navbar.Link>
         <Navbar.Link href="#">Contact</Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
